4bab248f6a89c598b5ec283a78823246937c19bb
Блинчик <vovius1@gmail.com>
2022-06-27 20:14:22 +0300

    Delete task_3.py

--- task_3.py
+++
@@ -1,8 +0,0 @@
-def zeros(n):
-    zeros_count = 0
-    if n:
-        i = 1
-        while((n / 5 ** i) >= 1):
-            zeros_count += int((n / 5 ** i))
-            i += 1
-    return zeros_count
