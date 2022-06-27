94aad3a933e0ddb3a7b141cc9e74ab4872e13d60
Блинчик <vovius1@gmail.com>
2022-06-27 20:14:07 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,7 +0,0 @@
-def domain_name(url):
-    if url.find('//') != -1:
-        url = url.split('//')[1]
-    if url.startswith('www'):
-        url = url.split('www.')[1]
-    url = url.split('.')[0]
-    return url
